e17b0dc143950ca53f7f9ace2b663dff0e9981c9
Brad Phillips <bjrp@protonmail.com>
2019-11-18 15:39:24 +0000

    Add formatting to rspec, completes user story 2

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,12 @@ describe Account do
   it 'is created with a balance of 0' do
     expect(account.balance).to eq 0
   end
+
+  describe '#deposit' do
+    it 'allows money to be stored in an account' do
+      account.deposit(100)
+      expect(account.balance).to eq 100
+    end
+  end
+
 end
