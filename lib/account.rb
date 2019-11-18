ce2ee4f4baefa5bda2bb50c8523abe13eef411e6
Brad Phillips <bjrp@protonmail.com>
2019-11-18 21:02:35 +0000

    Refactors to automatically enter date

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,6 @@
-class Account
+# frozen_string_literal: true
 
+class Account
   attr_reader :balance, :history
 
   def initialize
@@ -7,25 +8,27 @@ class Account
     @history = []
   end
 
-  def deposit(date, amount)
+  def deposit(amount)
     @balance += amount
-    store_deposit(date, amount)
+    store_deposit(amount)
   end
 
-  def withdraw(date, amount)
+  def withdraw(amount)
     @balance -= amount
-    store_withdrawal(date, amount)
+    store_withdrawal(amount)
   end
 
   private
 
-  def store_deposit(date, amount)
-    @history.push({date: "#{date}", deposit: amount, balance: @balance})
+  def store_deposit(amount)
+    @history.push(date: current_date, deposit: amount, balance: @balance)
   end
 
-  def store_withdrawal(date, amount)
-    @history.push({date: "#{date}", withdrawal: amount, balance: @balance})
+  def store_withdrawal(amount)
+    @history.push(date: current_date, withdrawal: amount, balance: @balance)
   end
 
-
+  def current_date
+    Date.today.strftime("%m/%d/%Y")
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require 'account'
 
 describe Account do
-
   subject(:account) { described_class.new }
+
+  date = Date.today.strftime("%m/%d/%Y")
+
   it 'is created with a balance of 0' do
     expect(account.balance).to eq 0
   end
@@ -12,28 +16,27 @@ describe Account do
 
   describe '#deposit' do
     it 'allows money to be stored in an account' do
-      account.deposit('11/11/19', 100)
+      account.deposit(100)
       expect(account.balance).to eq 100
     end
     it 'stores a record of the deposit' do
-      account.deposit('11/11/19', 100)
-      account.deposit('12/11/19', 100)
-      expect(account.history).to eq [{date: '11/11/19', deposit: 100, balance: 100}, {date: '12/11/19', deposit: 100, balance: 200}]
+      account.deposit(100)
+      account.deposit(100)
+      expect(account.history).to eq [{ date: date, deposit: 100, balance: 100 }, { date: date, deposit: 100, balance: 200 }]
     end
   end
 
   describe '#withdraw' do
     it 'allows money to be withdrawn from an account' do
-      account.deposit('11/11/19', 100)
-      account.withdraw('12/11/19', 100)
+      account.deposit(100)
+      account.withdraw(100)
       expect(account.balance).to eq 0
     end
     it 'stores a record of the withdrawal' do
-      account.deposit('11/11/19', 100)
-      account.withdraw('12/12/19', 50)
-      account.withdraw('13/12/19', 50)
-      expect(account.history).to eq [{date: '11/11/19', deposit: 100, balance: 100}, {date: '12/12/19', withdrawal: 50, balance: 50},  {date: '13/12/19', withdrawal: 50, balance: 0}]
+      account.deposit(100)
+      account.withdraw(50)
+      account.withdraw(50)
+      expect(account.history).to eq [{ date: date, deposit: 100, balance: 100 }, { date: date, withdrawal: 50, balance: 50 }, { date: date, withdrawal: 50, balance: 0 }]
     end
   end
-
 end
